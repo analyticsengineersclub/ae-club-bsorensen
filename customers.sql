f6328f538f9f1132d03aa27bb62a362dea3a5b27
Ubuntu <ubuntu@ip-172-31-31-19.us-east-2.compute.internal>
2021-08-25 22:14:24 +0000

    add new comment

--- customers.sql
+++ customers.sql
@@ -3,7 +3,8 @@ select
    customers.name as full_name, 
    customers.email as email_address, 
    count(distinct orders.id) as n_orders, 
-   min(orders.created_at) as first_order_at,
+   min(orders.created_at) as first_order_at
+   /** New comment **/
 from `analytics-engineers-club.coffee_shop.customers` customers 
 join `analytics-engineers-club.coffee_shop.orders` orders
   on customers.id = orders.customer_id 
